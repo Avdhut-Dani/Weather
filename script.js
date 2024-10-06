114e61c33f8438401d23ba7df23b90226b54f636
Avdhut_Dani <avdhutdani@gmail.com>
2024-10-06 08:18:37 +0530

    Initial commit
    My first attempt to create a fully functional website using HTML, CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,56 @@
+const apiKey = "53ff174a7a92d0fd632ad3e52c335c01";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+
+    var data = await response.json();
+
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "Clouds.png"
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "clear.png"
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "drizzle.png"
+    }
+    else if(data.weather[0].main == "Humidity"){
+        weatherIcon.src = "humidity.png"
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "mist.png"
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "rain.png"
+    }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "snow.png"
+    }
+    else if(data.weather[0].main == "Wind"){
+        weatherIcon.src = "wind.png"
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    }
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
